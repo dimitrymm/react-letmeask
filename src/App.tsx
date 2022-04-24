85a72d4826736c22e03237f23989460c0c20ac9d
Dimitry Machado Marinho <dimitrymmarinho@gmail.com>
2022-04-24 08:27:38 -0300

    Installation Node Sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Button } from './components/Button';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <Button />
-      <Button />
-    </div>
+    <Home />
   );
 }
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,35 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+
+
+export function Home() {
+  return (
+    <div>
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie Salas de Q&amp;A ao-vivo</strong>
+        <p>Tire suas duvidas em tempo real</p>
+      </aside>
+      <main>
+        <div>
+          <img src={logoImg} alt="Letmeask logotipo" />
+          <button>
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div>ou entre em uma sala</div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o Codigo da Sala"
+            />
+            <button type="submit">
+              Entrar na Sala
+            </button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
